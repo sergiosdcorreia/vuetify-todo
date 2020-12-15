8c217b09e26cdbc99225ca951f4918a9abd9a1d5
Sergio Correia <sergiosdcorreia@gmail.com>
2020-12-15 00:58:36 +0000

    No tasks message

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -14,6 +14,7 @@
     </v-text-field>
 
     <v-list
+      v-if="tasks.length"
       class="pt-0"
       flat
     >
@@ -51,6 +52,18 @@
         <v-divider></v-divider>
       </div>
     </v-list>
+    <div
+      v-else
+      class="no-tasks"
+    >
+      <v-icon
+        size="100"
+        color="primary"
+      >
+        mdi-check
+      </v-icon>
+      <div class="text-h5 primary--text">No tasks</div>
+    </div>
   </div>
 </template>
 
@@ -100,3 +113,12 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+  .no-tasks
+    position: absolute
+    left: 50%
+    top: 50%
+    transform: translate(-50%, -50%)
+    opacity: 0.5
+</style>
