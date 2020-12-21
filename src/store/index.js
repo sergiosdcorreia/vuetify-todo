38850824aa00cd06da24e4c09b0015db545e4221
Sergio Correia <sergiosdcorreia@gmail.com>
2020-12-21 19:44:30 +0000

    Adding due date to tasks

--- src/store/index.js
+++ src/store/index.js
@@ -9,17 +9,20 @@ export default new Vuex.Store({
       {
         id: 1,
         title: 'Wake up',
-        done: false
+        done: false,
+        dueDate: '2020-12-21'
       },
       {
         id: 2,
         title: 'Get bananas',
-        done: false
+        done: false,
+        dueDate: '2020-10-22'
       },
       {
         id: 3,
         title: 'Eat bananas',
-        done: false
+        done: false,
+        dueDate: null
       },
     ],
     snackbar: {
@@ -32,7 +35,8 @@ export default new Vuex.Store({
       let newTask = {
         id: Date.now(),
         title: newTaskTitle,
-        done: false
+        done: false,
+        dueDate: null
       }
       state.tasks.push(newTask)
     },
