0f790d7f92c8ea1f68127ef843e6252caee5e22b
Sergio Correia <sergiosdcorreia@gmail.com>
2020-12-30 20:10:46 +0000

    Content for about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="about pa-6">
-    <h1>About {{ $store.state.appTitle }}</h1>
-    <p>Lorem ipsum</p>
-    <p>Lorem ipsum</p>
-    <p>Lorem ipsum</p>
+  <div class="pa-6">
+    <h1 class="mb-3">About {{ $store.state.appTitle }}</h1>
+    <p>{{ $store.state.appTitle }} developed by Sergio Correia</p>
+    <p>December 2020</p>
   </div>
 </template>
