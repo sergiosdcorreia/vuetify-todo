28799e315c278a4a58b90f2b1178fdefce2d134e
Sergio Correia <sergiosdcorreia@gmail.com>
2020-12-30 06:13:21 +0000

    Disabling sorting while searching

--- src/components/Todo/TaskMenu.vue
+++ src/components/Todo/TaskMenu.vue
@@ -60,7 +60,11 @@ export default {
         title: 'Sort',
         icon: 'mdi-drag-horizontal-variant',
         click() {
-          this.$store.commit('toggleSorting')
+          if (!this.$store.state.search) {
+            this.$store.commit('toggleSorting')
+          } else {
+            this.$store.commit('showSnackbar', 'Unable to sort while searching')
+          }
         }
       }
     ],
